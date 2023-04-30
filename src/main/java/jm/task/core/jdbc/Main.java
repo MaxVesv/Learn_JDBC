02671e15d5fd276d309cef5bc1aa7f3c5266e1cb
Maxim <vesvalomn@gmail.com>
2023-05-01 00:09:15 +0400

    REFACTOR (HIBERNATE): DELETE ALL ABOUT HIBERNATE LIKE IN TASK

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,6 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.sql.SQLException;
@@ -41,38 +40,6 @@ public class Main {
 
        /**  Удаление таблицы */
        userDaoJDBC.dropUsersTable();
-
-        /**
-         *  ********************************************************************************************
-         *  ПРИМЕР РЕАЛИЗАЦИИ С HIBERNATE***************************************************************
-         *  ********************************************************************************************
-         */
-
-        UserDao connect = UserDaoHibernateImpl.getDaoInstanceHubernate();
-
-        /**  Создание таблицы User(ов) */
-        connect.createUsersTable();
-
-        /**  Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления
-         *  должен быть вывод в консоль ( User с именем – name добавлен в базу данных */
-        connect.saveUser("Ivan", "Petrov", (byte)13);
-        connect.saveUser("Sergey", "Suvorov", (byte)16);
-        connect.saveUser("Maxim", "Ivanov", (byte)45);
-        connect.saveUser("Petr", "Sidorov", (byte)73);
-//
-        /**  Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User) */
-        List<User> listUser = connect.getAllUsers();
-        System.out.println("ВЫВОД ВЫВОД ПОЛУЧЕННЫХ ДАННЫХ!");
-        list.stream().forEach (a -> System.out.println(a.toString()));
-//
-        //удаление по id
-//        connect.removeUserById(4);
-//
-//        /**  Очистка таблицы User(ов) */
-//        connect.cleanUsersTable();
-//
-//        /**  Удаление таблицы */
-//        connect.dropUsersTable();
 
     }
 }
